a1cb627f065c355ea0766a5ad9b222ab6df61733
importantcoding <36238285+importantcoding@users.noreply.github.com>
2018-04-05 15:45:54 -0400

    updated fizzbuzz to a cleaner delivery

--- excercises/fizzbuzz.go
+++ excercises/fizzbuzz.go
@@ -1,21 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	var fizz string
 	var buzz string
 	for i := 1; i <= 100; i++ {
 		if i%3 == 0 {
-			fizz = "fizz"
+			fizz = "Fizz"
 		}
 		if i%5 == 0 {
-			buzz = "buzz"
+			buzz = "Buzz"
 		}
 
-		fmt.Println(i, fizz, buzz)
+		fmt.Println(i, fizz+buzz)
 
 		fizz = ""
 		buzz = ""
