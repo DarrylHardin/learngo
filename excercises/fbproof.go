9ec454cb706b1c6f07d70937d322474794d9c9cf
importantcoding <36238285+importantcoding@users.noreply.github.com>
2018-04-09 09:02:18 -0400

    fizzbuzz update

--- excercises/fbproof.go
+++ excercises/fbproof.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	// I was told this code wouldn't work with 5 and 10, I ran the code to find out.
 	for i := 1; i <= 100; i++ {
 		var f string
 		var b string
-		if i%3 == 0 {
+		if i%5 == 0 {
 			f = "Fizz"
 		}
-		if i%5 == 0 {
+		if i%10 == 0 {
 			b = "Buzz"
 		}
 
