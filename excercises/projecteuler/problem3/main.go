87fa0a4538c3baa6d9f08f56f0e92d58f71ef4ac
importantcoding <36238285+importantcoding@users.noreply.github.com>
2018-04-12 11:45:33 -0400

    commit

--- excercises/projecteuler/problem3/main.go
+++ excercises/projecteuler/problem3/main.go
@@ -3,27 +3,26 @@ package main
 import "fmt"
 
 func main() {
-
-	check := 13195
-	var highest int
-	for i := 2; i < check; i++ {
+	check := int64(600851475143)
+	var highest int64
+	for i := check / int64(3); i < check/int64(2); i++ {
 		switch {
 		case i%2 == 0 && i != 2:
 			continue
 		case i%3 == 0 && i != 3:
 			continue
-		case i%4 == 0 && i != 4:
-			continue
 		case i%5 == 0 && i != 5:
-			continue
-		case i%6 == 0 && i != 6:
 			continue
 		case i%7 == 0 && i != 7:
 			continue
-		case i%8 == 0 && i != 8:
+		case i%11 == 0 && i != 11:
 			continue
-		case i%9 == 0 && i != 9:
+		case i%13 == 0 && i != 13:
 			continue
+		// case i%8 == 0 && i != 17:
+		// 	continue
+		// case i%9 == 0 && i != 19:
+		// 	continue
 		default:
 			if check%i == 0 {
 				if i > highest {
