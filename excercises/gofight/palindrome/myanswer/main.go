e714c8e7065f289aa95054c5fb114b8a83e2629b
importantcoding <darrylrhardin@gmail.com>
2018-08-07 13:50:55 -0400

    updating files

--- excercises/gofight/palindrome/myanswer/main.go
+++ excercises/gofight/palindrome/myanswer/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
 func checkPalindrome(inputString string) bool {
-    f := strings.Split(inputString, "")
-    x := len(f)
-    b := make([]string, x)
-    copy(b[:], f)
-    i := len(f)
-    for _, y := range f {
-        i--
-        b[i] = y
-    }
-    o := strings.Join(f, "")
-    r := strings.Join(b, "")
+	f := strings.Split(inputString, "")
+	x := len(f)
+	b := make([]string, x)
+	copy(b[:], f)
+	i := len(f)
+	for _, y := range f {
+		i--
+		b[i] = y
+	}
+	o := strings.Join(f, "")
+	r := strings.Join(b, "")
 
-    return o == r
+	return o == r
 }
 
 func main() {
-    x := "aabba"
-    y := "lol"
-    fmt.Println(checkPalindrome(x))
-    fmt.Println(checkPalindrome(y))
+	x := "aabba"
+	y := "lol"
+	fmt.Println(checkPalindrome(x))
+	fmt.Println(checkPalindrome(y))
 }
--- firstduplicate/routine.go
+++ firstduplicate/routine.go
@@ -3,46 +3,46 @@ package main
 import "fmt"
 
 func firstDuplicate(a []int) int {
-    x := len(a) - 1
-    var y bool
-    c := make(chan int)
-    go func() {
-        for num, key := range a {
-            exist := key
-            for i := num + 1; i < len(a); i++ {
-                if exist == a[i] {
-                    c <- i
-                }
-            }
-        }
-        close(c)
-    }()
+	x := len(a) - 1
+	var y bool
+	c := make(chan int)
+	go func() {
+		for num, key := range a {
+			exist := key
+			for i := num + 1; i < len(a); i++ {
+				if exist == a[i] {
+					c <- i
+				}
+			}
+		}
+		close(c)
+	}()
 
-    for n := range c {
-        if x > n {
-            x = n
-            y = true
-        } else if len(a) <= 3 {
-            x = n
-            y = true
-        }
-    }
+	for n := range c {
+		if x > n {
+			x = n
+			y = true
+		} else if len(a) <= 3 {
+			x = n
+			y = true
+		}
+	}
 
-    if y == true {
-        return a[x]
-    } else {
-        return -1
-    }
+	if y == true {
+		return a[x]
+	} else {
+		return -1
+	}
 }
 
 func main() {
 
-    a := []int{4, 3, 3, 5, 4}       //4
-    b := []int{5, 6, 5, 6}          //5
-    c := []int{3, 4, 1, 4, 5, 8, 8} //4
-    d := []int{2, 3, 3, 1, 5, 2}    //3
-    fmt.Println(firstDuplicate(a))
-    fmt.Println(firstDuplicate(b))
-    fmt.Println(firstDuplicate(c))
-    fmt.Println(firstDuplicate(d))
+	a := []int{4, 3, 3, 5, 4}       //4
+	b := []int{5, 6, 5, 6}          //5
+	c := []int{3, 4, 1, 4, 5, 8, 8} //4
+	d := []int{2, 3, 3, 1, 5, 2}    //3
+	fmt.Println(firstDuplicate(a))
+	fmt.Println(firstDuplicate(b))
+	fmt.Println(firstDuplicate(c))
+	fmt.Println(firstDuplicate(d))
 }
